613aa1482f7393ac75c323597906626552773d6d
anurag7276 <anurag7276singh@gmail.com>
2025-08-08 10:28:54 +0530

    port

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 10000;
 
 // Call the new configurePassport function to set up Passport
 const passport = configurePassport();
